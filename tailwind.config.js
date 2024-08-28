549d231e9ed0a9496a076077f7276d9345d4f20b
Kauan <raidoka546@gmail.com>
2024-08-28 11:11:06 -0400

    removing unused vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,18 +8,11 @@ export default {
     colors: {
       white: '#FFFFFF',
       zinc50: '#fafafa',
-      zinc100: '#f4f4f5',
-      zinc200: '#e4e4e7',
       zinc300: '#d4d4d8',
-      zinc400: '#a1a1aa',
       zinc500: '#71717a',
-      zinc600: '#52525b',
-      zinc700: '#3f3f46',
-      zinc800: '#27272a',
-      zinc900: '#18181b',
       zinc950: '#09090b',
       violet800: '#5b21b6',
     },
   plugins: [],
-}
+  }
 }
