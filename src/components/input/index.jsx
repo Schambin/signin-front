db5c7be675fae822f29a98984da3dbb1c4a5a8a6
Kauan <raidoka546@gmail.com>
2024-08-28 11:15:11 -0400

    removed input comp.

--- src/components/input/index.jsx
+++
@@ -1,7 +0,0 @@
-export function Input( props ) {
-    return (
-        <>
-            <input {...props} className="border-2 border-zinc500 rounded-md px-4 py-5 w-[420px] mt-4" />
-        </>
-    )    
-}
