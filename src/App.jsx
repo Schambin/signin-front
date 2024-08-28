cc800c0e922ee4db32e9c977057baa778754bd35
Kauan <raidoka546@gmail.com>
2024-08-28 11:13:51 -0400

    Path to list users

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import { RegisterUser } from './pages/register';
+import { Register } from './pages/register';
 import { Login } from './pages/login';
+import { ListUsers } from './pages/user-list';
 
 export function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<RegisterUser />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/" element={<Login />} />
+        <Route path="/p/list-users" element={<ListUsers />} />
       </Routes>
     </BrowserRouter>
   )
 }
-  
